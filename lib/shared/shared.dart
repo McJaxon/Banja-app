9fa6f0f2ab504cf253cdbcf96369284df88dc68d
Ronnie Zad <zadcorna@gmail.com>
2022-06-19 13:44:37 +0300

    refactored code

---
+++ lib/shared/shared.dart
@@ -0,0 +1,9 @@
+export 'clippers.dart';
+export 'headers.dart';
+export 'loading_indicator.dart';
+export 'network_error.dart';
+export 'no_record_error.dart';
+export 'notifications.dart';
+export 'side_drawer.dart';
+export 'text_box.dart';
+
